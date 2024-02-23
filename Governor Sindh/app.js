e96a276e5dc44eb4edd081fd6b3fea808ac91180
ABDULHADI18 <160162625+ABDULHADI18@users.noreply.github.com>
2024-02-23 16:28:46 +0500

    Initial commit

---
+++ Governor Sindh/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let message = "Thankyou Governor Sindh Kamran Khan Tessori"
+console.log(message);
---
+++ Governor Sindh/app.ts
@@ -0,0 +1,2 @@
+let message = "Thankyou Governor Sindh Kamran Khan Tessori"
+console.log(message);
